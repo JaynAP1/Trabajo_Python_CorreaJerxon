5be4cb55beb8c47a874b61b0adcfdaf4e1db77b2
JaynAP1 <jerxonmjair@gmail.com>
2024-08-08 02:25:46 -0500

    Update: :white_check_mark: Funcionalidad terminada

--- Main.py
+++ Main.py
@@ -2,17 +2,20 @@ import json
 import datetime
 import os
 
-with open("Info.json", encoding="utf-8") as openfile:
-    PrecioPan=json.load(openfile)
 
-with open("DatosVentas.json", encoding="utf-8") as openVenta:
-    Ventas=json.load(openVenta)
-
-with open("DatosCompra.json", encoding="utf-8") as openCompra:
-    Compras=json.load(openVenta)
 
 Bol=True
 while Bol==True:
+
+    with open("Info.json", encoding="utf-8") as openfile:
+        PrecioPan=json.load(openfile)
+
+    with open("DatosVentas.json", encoding="utf-8") as openVenta:
+        Ventas=json.load(openVenta)
+
+    with open("DatosCompra.json", encoding="utf-8") as openCompra:
+        Compras=json.load(openCompra)
+
     os.system("clear")
     print("==========================\n1).Vender\n2).Comprar\n3).Ver registros\n4).Salir\n==========================")
     Opcion1=input(str("Ingrese un numero para ir a la opcion deseada: "))
@@ -205,10 +208,65 @@ while Bol==True:
             print("==========================\n1).Comprar\n2).Salir\n==========================")
             Opcion2=input(str("Ingrese un numero para ir a la opcion deseada: "))
             if Opcion2=="1":
-                input("Presione Enter para continuar")
+                NombreProovedor=str(input("Nombre del proovedor al que se le comprara: "))
+                NumeroProovedor=int(input("Numero del proovedor al que desea comprar: "))
+                NombreProducto=str(input("Que producto desea comprar: "))
+                CantidadProducto=int(input("Cantidad del producto que desea comprar: "))
+                PrecioProducto=int(input("Precio del producto que desea comprar: "))
+                PrecioTotal=PrecioProducto*CantidadProducto
+                Fecha=str(datetime.datetime.now())
+
+                Compras[0]["Personas"].append(
+                    {
+                        "Fecha": Fecha,
+                        "Nombre":NombreProovedor,
+                        "Numero":NumeroProovedor,
+                        "Producto":NombreProducto,
+                        "Cantidad":CantidadProducto,
+                        "Precio Unitario":PrecioProducto,
+                        "Precio Total":PrecioTotal
+                    }
+                )
+                with open("DatosCompra.json", "w") as files:
+                    json.dump(Compras,files)
 
             elif Opcion2=="2":
+                os.system("clear")
+                print("======Saliendo======")
+                input("Presione Enter para continuar")
                 bol1=False
+
+    elif Opcion1=="3":
+        bol2=True
+        while bol2==True:
+            os.system("clear")
+            print("==========================\n1).Registros ventas\n2).Registros Compras\n3).Salir\n==========================")         
+            Opcion2=input(str("Ingrese un numero para ir a la opcion deseada: "))
+
+            if Opcion2=="1":
+                os.system("clear")
+                for i in Ventas[0]["Personas"]:
+                    print("================Ventas==========================")
+                    print("Fecha de compra: ",i["Fecha"],"\nDireccion: ",i["Nombre"],"\nEmpleado: ",i["Empleado"],"\nProducto: ",i["Producto"][0]["NombreP"],"\nCantidad: ",i["Producto"][0]["Cantidad"],"\nPrecio Venta : ",i["Producto"][0]["PrecioP"])
+                    print("================================================")
+
+                input("Presione Enter para continuar")    
+
+            elif Opcion2=="2":
+                os.system("clear")
+                for i in Compras[0]["Personas"]:
+                    print("==================Compras========================")
+                    print("Fecha de compra: ",i["Fecha"],"\nNombre del proovedor: ",i["Nombre"],"\nProducto: ",i["Producto"],"\nCantidad: ",i["Cantidad"],"\nPrecio Total: ",i["Precio Total"])
+                    print("=================================================")
+
+                input("Presione Enter para continuar")
+
+            elif Opcion2=="3":
+                os.system("clear")
+                print("======Saliendo======")
+                input("Presione Enter para continuar")
+                bol2=False
+
 
     elif Opcion1=="4":
         Bol=False
